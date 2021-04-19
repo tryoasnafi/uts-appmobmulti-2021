ff4b2bc0c9b181694cc5514493e9a1d3581f2de7
Tryo Asnafi <tryoasnafi@gmail.com>
2021-04-19 09:14:30 +0700

    add dynamic input biodata ketua himpunan

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,19 @@
+import 'dart:io';
 import 'ketua_hima.dart';
 
 void main(List<String> arguments) {
   var ketuaHima = KetuaHima();
 
-  ketuaHima.setNrp(6304191206);
-  ketuaHima.setNama('Tryo Asnafi');
-  ketuaHima.setJurusan('Teknik Informatika');
+  stdout.write('Input NRP: ');
+  var nrp = int.tryParse(stdin.readLineSync());
+  stdout.write('Input Nama: ');
+  var nama = stdin.readLineSync();
+  stdout.write('Input Jurusan: ');
+  var jurusan = stdin.readLineSync();
+
+  print('');
+  ketuaHima.setNrp(nrp);
+  ketuaHima.setNama(nama);
+  ketuaHima.setJurusan(jurusan);
   ketuaHima.info();
 }
