0f6d1f5e2c164ca60dfcc32a57c8247485fc077d
Tryo Asnafi <tryoasnafi@gmail.com>
2021-04-19 08:55:17 +0700

    assign variable and call method info()

--- bin/main.dart
+++ bin/main.dart
@@ -1,3 +1,10 @@
+import 'ketua_hima.dart';
+
 void main(List<String> arguments) {
-  print('Hello World!');
+  var ketuaHima = KetuaHima();
+
+  ketuaHima.setNrp(6304191206);
+  ketuaHima.setNama('Tryo Asnafi');
+  ketuaHima.setJurusan('Teknik Informatika');
+  ketuaHima.info();
 }
